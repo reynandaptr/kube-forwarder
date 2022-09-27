5901f085078bf69480178fbe9d9323234ec5e779
reynandaptr <reynandapp1997@gmail.com>
2022-09-27 22:47:22 +0700

    feat: disable google analytics

--- src/renderer/analytics.js
+++ src/renderer/analytics.js
@@ -1,8 +1,11 @@
 let analytics
 
 if (!process.env.IS_WEB) {
-  const Analytics = require('electron-ga').default
-  analytics = new Analytics(process.env.GA_TRACKING_ID)
+  // const Analytics = require('electron-ga').default
+  // analytics = new Analytics(process.env.GA_TRACKING_ID)
+  analytics = {
+    send: () => {}
+  }
 } else {
   analytics = {
     send: () => {}
